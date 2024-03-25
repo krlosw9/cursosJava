cdcac557843d92e8917e97ecff3b4dd6ee63ae6c
Carlos Waldo Rojas <krlosw9@gmail.com>
2024-03-25 12:21:40 -0500

    Getter y Setter

--- JavaSEOrientadoAObjetos/src/Patient.java
+++ JavaSEOrientadoAObjetos/src/Patient.java
@@ -1,4 +1,5 @@
 public class Patient {
+    private static int id;
     private String name;
     private String email;
     private String address;
@@ -11,5 +12,84 @@ public class Patient {
     Patient(String name, String email){
         this.name   = name;
         this.email  = email;
+        id++;
     }
+
+    //Setter -> son para validar la informacion que ingresa al objeto
+    //Getter -> son para dar formato a la informacion que retorna del objeto 
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        if (phoneNumber.length() == 0) {
+            this.phoneNumber = phoneNumber;
+        }else{
+            System.out.println("El numero de telefono debe ser de 8 caracteres");
+        }
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    // 54.5 Kg.
+    public String getWeight() {
+        return weight+" Kg.";
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public String getBlood() {
+        return blood;
+    }
+
+    public void setBlood(String blood) {
+        this.blood = blood;
+    }
+
+    
 }
