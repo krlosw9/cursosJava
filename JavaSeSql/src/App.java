850f35c140b057c2758230df216f9fce506c5153
Carlos Waldo Rojas <krlosw9@gmail.com>
2024-03-27 12:14:51 -0500

    Crear datos con JDBC - java-sql

--- JavaSeSql/src/App.java
+++ JavaSeSql/src/App.java
@@ -1,20 +1,34 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
+// import java.sql.ResultSet;
+// import java.sql.Statement;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Connection myConn = null;
-        Statement myStamt = null;
-        ResultSet myRes = null;
+        // Statement myStamt = null;
+        PreparedStatement myPtamt = null;
+        // ResultSet myRes = null;
         try {
             myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/project", "krlos", "//654321");
+            /*
+             * Para Select 
             myStamt = myConn.createStatement();
             myRes = myStamt.executeQuery("SELECT * FROM employees");
 
             while (myRes.next()) {
                 System.out.println(myRes.getString("first_name"));
+            }*/
+            String sql = "INSERT INTO employees (first_name, pa_surname) VALUES (?, ?)";
+            myPtamt = myConn.prepareStatement(sql);
+            myPtamt.setString(1, "Johana");
+            myPtamt.setString(2, "Dorantes");
+
+            int rowsAffected = myPtamt.executeUpdate();
+
+            if(rowsAffected > 0){
+                System.out.println("Se ha creado un nuevo cliente");
             }
             
         } catch (Exception e) {
