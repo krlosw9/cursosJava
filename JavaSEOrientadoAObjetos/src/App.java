7771c832a6853700361fef1caf98953c0cbfdf96
Carlos Waldo Rojas <krlosw9@gmail.com>
2024-03-27 06:50:46 -0500

    UI Menu - oop

--- JavaSEOrientadoAObjetos/src/App.java
+++ JavaSEOrientadoAObjetos/src/App.java
@@ -1,22 +1,17 @@
-// import static ui.UIMenu.*;
+import static ui.UIMenu.*;
 
-import java.util.Date;
+/*import java.util.Date;
 
 import model.Doctor;
 import model.ISchedulable;
 import model.Patient;
-import model.User;
+import model.User;*/
 
 public class App {
     public static void main(String[] args) throws Exception {
-        //showMenu();
+        showMenu();
 
-        Doctor myDoctor = new Doctor("Anahi Salgado", "ana@mail.com","Pediatria");
-        System.out.println(myDoctor);
-
-        Patient myPatient = new Patient("Paciente1", "paciente1@gmail.com");
-        System.out.println(myPatient);
-
+        /*
         User anonymousUser = new User("Usuario anonimo", "anonimo@mail.com") {
             @Override
             public void showDataUser(){
@@ -34,5 +29,6 @@ public class App {
             }
         };
         iSchedulable.schedule(null, null);
+        */
     }
 }
