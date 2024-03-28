14f937b8440c6ab35105128df8d5f75a635a6496
Carlos Waldo Rojas <krlosw9@gmail.com>
2024-03-28 07:20:40 -0500

    Clases y estructura del proyecto con Maven - java-sql

---
+++ JavaSeSql/crud-maven/src/main/java/org/example/platzi/Main.java
@@ -0,0 +1,26 @@
+package org.example.platzi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) {
+        Connection myConn = null;
+        Statement myStamt = null;
+        ResultSet myRes = null;
+        try {
+            myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/project", "krlos", "//654321");
+            myStamt = myConn.createStatement();
+            myRes = myStamt.executeQuery("SELECT * FROM employees");
+
+            while (myRes.next()) {
+                System.out.println(myRes.getString("first_name"));
+            }
+        } catch (Exception e) {
+            System.out.println("Algo salio mal...");
+            e.printStackTrace();
+        }
+    }
+}
