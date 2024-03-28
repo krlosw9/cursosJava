4d3aff7fe4b7e1c6203ae9fb17e2740dae30ee68
Carlos Waldo Rojas <krlosw9@gmail.com>
2024-03-28 08:15:39 -0500

    Optimización de recursos (Try-with resources)

--- JavaSeSql/crud-maven/src/main/java/org/example/platzi/Main.java
+++ JavaSeSql/crud-maven/src/main/java/org/example/platzi/Main.java
@@ -7,13 +7,12 @@ import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
-        Connection myConn = null;
-        Statement myStamt = null;
-        ResultSet myRes = null;
-        try {
-            myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/project", "krlos", "//654321");
-            myStamt = myConn.createStatement();
-            myRes = myStamt.executeQuery("SELECT * FROM employees");
+        String url = "jdbc:mysql://localhost:3306/project";
+        String user = "krlos";
+        String pass = "//654321";
+        try (Connection myConn = DriverManager.getConnection(url, user, pass);
+            Statement myStamt = myConn.createStatement();
+            ResultSet myRes = myStamt.executeQuery("SELECT * FROM employees");) {
 
             while (myRes.next()) {
                 System.out.println(myRes.getString("first_name"));
@@ -21,20 +20,6 @@ public class Main {
         } catch (Exception e) {
             System.out.println("Algo salio mal...");
             e.printStackTrace();
-        } finally {
-            try {
-                if (myConn != null) {
-                    myConn.close();
-                }
-                if (myStamt != null) {
-                    myStamt.close();
-                }
-                if (myRes != null) {
-                    myRes.close();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
         }
     }
 }
