54f1cd14cf0ba0667dc55aa1d612829d58b54fef
Carlos Waldo Rojas <krlosw9@gmail.com>
2024-03-30 10:01:38 -0500

    Testing-java | Instalación de IntelliJ IDEA, creación del Proyecto con Maven y Tests Unitarios

---
+++ BasicoTesting/java-test/src/test/java/com/platzi/javatest/util/StringUtilTest.java
@@ -0,0 +1,8 @@
+package com.platzi.javatest.util;
+
+public class StringUtilTest {
+    public static void main(String[] args) {
+        String result = StringUtil.repeat("hola",3);
+        System.out.println(result);
+    }
+}
